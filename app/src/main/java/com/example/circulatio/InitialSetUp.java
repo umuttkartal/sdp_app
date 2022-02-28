999ccdf529ce29fde7b2fec2c2a809c4b4c01126
Skaiste <s1972149@ed.ac.uk>
2022-02-28 17:39:48 +0000

    Implement UI screens of the first implementation stage

---
+++ app/src/main/java/com/example/circulatio/InitialSetUp.java
@@ -0,0 +1,14 @@
+package com.example.circulatio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InitialSetUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_set_up);
+    }
+}
